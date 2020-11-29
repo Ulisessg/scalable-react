8ef8b7542e8ce93af62ecf2a8927e0364f0af31c
Ulisessg <ukisessg@gmail.com>
2020-11-29 17:34:57 -0600

    [FIX] Removed burned data

---
+++ src/data/directories.js
@@ -0,0 +1,21 @@
+const directories = [
+  'assets/icons',
+  'assets/images',
+  'public',
+  //react
+  'src/react',
+  'src/react/atoms',
+  'src/react/molecules',
+  'src/react/organisms',
+  'src/react/templates',
+  'src/react/pages',
+  //Styles
+  'src/styles',
+  'src/styles/atoms',
+  'src/styles/molecules',
+  'src/styles/organisms',
+  'src/styles/templates',
+  'src/styles/pages',
+];
+
+module.exports = directories;
--- src/data/index.js
+++ src/data/index.js
@@ -1,39 +1,7 @@
 //@ts-check
-const { join } = require('path');
 
-const directories = [
-  'assets/icons',
-  'assets/images',
-  'public',
-  //react
-  'src/react',
-  'src/react/atoms',
-  'src/react/molecules',
-  'src/react/organisms',
-  'src/react/templates',
-  'src/react/pages',
-  //Styles
-  'src/styles',
-  'src/styles/atoms',
-  'src/styles/molecules',
-  'src/styles/organisms',
-  'src/styles/templates',
-  'src/styles/pages',
-];
-
-const indexHtml = [
-  join(process.cwd(), 'public'),
-  `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Scalable react app</title>
-  </head>
-  <body></body>
-</html>
-`,
-];
+const indexHtml = require('./templates/indexHtml');
+const directories = require('./directories');
 
 module.exports = {
   directories,
