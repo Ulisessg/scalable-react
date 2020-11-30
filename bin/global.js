9abb9bdf82a5ff3b7bcb3f33fe7881383d592a5c
Ulisessg <ukisessg@gmail.com>
2020-11-30 01:39:22 -0600

    [ADD] Index.js template

--- bin/global.js
+++ bin/global.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 //@ts-check
 
+const { join } = require('path');
+
 //Data
 const { directories } = require('../src/data/index');
 
@@ -15,7 +17,13 @@ const { mkdirs, copyFile } = require('../src/index');
     //Create index.html
     await copyFile(
       '../src/templates/index.html',
-      `${process.cwd()}/public/index.html`,
+      join(process.cwd(), 'public', 'index.html'),
+    );
+
+    //Create index.js
+    await copyFile(
+      '../src/templates/index.js',
+      join(process.cwd(), 'src', 'react', 'pages', 'index.js'),
     );
   } catch (error) {
     throw error;
