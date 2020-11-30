1a3be720e5e9243ff13416b121ee58667f5ddf39
Ulisessg <ukisessg@gmail.com>
2020-11-30 03:00:29 -0600

    [ADD] Detection of init command and develop command, copied webpack.dev.js file

--- bin/global.js
+++ bin/global.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 //@ts-check
 
+//Modules
 const { join } = require('path');
+
+const action = process.argv[2];
 
 //Data
 const { directories } = require('../src/data/index');
@@ -9,7 +12,7 @@ const { directories } = require('../src/data/index');
 //Utils
 const { mkdirs, copyFile } = require('../src/index');
 
-(async function main() {
+async function init() {
   try {
     //Make directiores
     await mkdirs(directories);
@@ -42,7 +45,32 @@ const { mkdirs, copyFile } = require('../src/index');
       '../src/templates/.eslintrc',
       join(process.cwd(), '.eslintrc'),
     );
+
+    //package.json
+    await copyFile(
+      '../src/templates/package.json',
+      join(process.cwd(), 'package.json'),
+    );
+    await copyFile(
+      '../src/templates/webpack.dev.js',
+      join(process.cwd(), 'webpack.dev.js'),
+    );
   } catch (error) {
     throw error;
   }
-})();
+}
+
+function develop() {
+  //
+}
+
+switch (action) {
+  case 'init':
+    init();
+    break;
+  case 'develop':
+    develop();
+    break;
+  default:
+    throw new Error(`This command doesn't exist`);
+}
